e6b2b3074d6dbc032b1104c80eb40e584642b42f
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-03-29 00:37:14 +0530

    CSS Updated

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
-define('ROOT_URL', 'https://localhost/Blog/');
-define('DB_HOST','localhost');
-define('DB_USER', 'swapnil');
-define('DB_PASSWORD', 'Swapnil@340340');
-define('DB_NAME', 'blog');
+// define('ROOT_URL', 'https://localhost/Blog/');
+// define('DB_HOST','localhost');
+// define('DB_USER', 'swapnil');
+// define('DB_PASSWORD', 'Swapnil@340340');
+// define('DB_NAME', 'blog');
--- admin/config/database.php
+++ admin/config/database.php
@@ -1,2 +1,2 @@
 <?php
-require 'constants.php';
+// require 'constants.php';
